a1d7a7a069e558af8426b7f3580852147c56aa39
David <71563428+Darodbas@users.noreply.github.com>
2022-04-23 16:57:57 +0200

    Implementacion de conexion al micro (Envio de datos)

--- app/src/main/java/com/example/controlbldc/MainActivity.java
+++ app/src/main/java/com/example/controlbldc/MainActivity.java
@@ -9,16 +9,22 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
 
@@ -27,14 +33,19 @@ public class MainActivity extends AppCompatActivity {
 
     protected int REQUEST_ENABLE_BT = 1;
     protected int numDisp = 0;
+
     protected String dispositivosEmp = "";
     protected String macMicro = "";
     protected String nombreMicro = "DAVID-PC";
 
-    protected Button btBoton, btPaired, btBuscar, btConectar;
-    public TextView tvMensaje, tvDisp, tvMAC;
+    protected Button  btConectar,btEnvio,btRecibe;
+    public TextView tvRecibo;
+    protected EditText etEnvio;
 
-    protected UUID myUUID = fromString("9875E838-C3B2-CCC2-735D-0C9D92C2D64A");
+    protected boolean conectado=false;
+
+
+    protected UUID mUUID = fromString("00001101-0000-1000-8000-00805F9B34FB");
 
 
     Intent enableBtIntent; // intent para activar bluetooth
@@ -44,13 +55,56 @@ public class MainActivity extends AppCompatActivity {
 
     public BluetoothAdapter bluetoothAdapter = null;
 
-    protected BluetoothServerSocket mmSocket;
-    protected BluetoothDevice mmDevice;
-    protected BluetoothSocket tmp;
+    protected BluetoothSocket btsocket = null;
+
+    OutputStream salidas;
+    InputStream entradas;
+
+
+    public void leerEntradas(){
+
+
+
+
+    }
+
+    public void MensajesPantalla(int codigo){
+
+        String mensaje=null;
+
+        Context context = getApplicationContext();
+        Toast mensajePant;
+        switch (codigo){
+
+            case 0:
+                mensaje="Dispositivo NO conectado";
+                break;
+
+            case 1:
+                mensaje="Campo de envío vacio";
+                break;
+
+            case 3:
+                mensaje="Bluetooth no soportado";
+                break;
+
+            case 4:
+                mensaje="Error al conectar";
+                break;
+
+
+        }
+        mensajePant = Toast.makeText(context,mensaje,Toast.LENGTH_SHORT);
+        mensajePant.setGravity(Gravity.BOTTOM,0,0);
+        mensajePant.show();
+
+
+    }
+
 
 
     @SuppressLint("MissingPermission")
-    protected void PairedDisp() { //crea pairedDevices y el String dispositivos y comprueba si alguno es el microcontrolador//
+    public void PairedDisp() { //crea pairedDevices y el String dispositivos y comprueba si alguno es el microcontrolador//
         pairedDevices = bluetoothAdapter.getBondedDevices();
         numDisp = 0;
         dispositivosEmp = "";
@@ -65,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
                 if (deviceName.equals(nombreMicro)) {
                     macMicro = deviceHardwareAddress;
                     nombreMicro = deviceName;
-                    microControlador = device;
+                    microControlador = bluetoothAdapter.getRemoteDevice(macMicro);
                 }
 
                 dispositivosEmp = dispositivosEmp + "\r\n" + deviceName;
@@ -75,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         switch (requestCode) {
@@ -83,10 +137,8 @@ public class MainActivity extends AppCompatActivity {
             case 1:
                 if (resultCode == RESULT_OK) {
                     //bluetooth permitido
-                    tvMensaje.setText("Bluetooth activado");
                 } else if (resultCode == RESULT_CANCELED) {
                     //bluetooth no permitido
-                    tvMensaje.setText("Bluetooth sigue inactivo");
                 }
 
         }
@@ -98,128 +150,162 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btBoton = findViewById(R.id.btBoton);
-        btPaired = findViewById(R.id.btPaired);
-        btBuscar = findViewById(R.id.btBuscar);
         btConectar = findViewById(R.id.btConectar);
-        tvMensaje = findViewById(R.id.tvMensaje);
-        tvDisp = findViewById(R.id.tvDisp);
-        tvMAC = findViewById(R.id.tvMAC);
+        etEnvio = findViewById(R.id.etTextoAEnviar);
+        btEnvio = findViewById(R.id.btEnviar);
+        tvRecibo = findViewById(R.id.tvRecibe);
+        btRecibe = findViewById(R.id.btRecibir);
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-
+        //Comprobaciuones iniciales//
         if (bluetoothAdapter == null) {
             // No soporta bluetooth
-            tvMensaje.setText("Bluetooth no soprtado");
+           MensajesPantalla(3);
         }
 
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             //No soporta BLE
-            Toast.makeText(this, "R.string.ble_not_supported", Toast.LENGTH_SHORT).show();
+            MensajesPantalla(3);
         }
 
-        btBoton.setOnClickListener(new View.OnClickListener() {
+        btConectar.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
             @Override
             public void onClick(View view) {
-
+                boolean btactiv;
+                //Comprueba que Bt este activado y pide permiso en caso de no estarlo//
                 if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
                     enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                     startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                    btactiv=false;
                 } else {
-                    tvMensaje.setText("Bluetooth activo");
+
+                    btactiv=true;
+                }
+
+                if(btactiv) {
+                    if (!conectado) {
+                        //Luego busca el micro en los emparejados//
+                        PairedDisp();
+
+                        //intenta conectarse//
+                        if (macMicro.length() > 0) {
+
+                            int contador = 0;
+
+                            do {
+
+
+                                try {
+
+                                    btsocket = microControlador.createRfcommSocketToServiceRecord(mUUID);
+
+                                    btsocket.connect();
+
+
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                                contador++;
+                            } while (!btsocket.isConnected() && contador <= 10);
+
+                            if (btsocket.isConnected()) {
+                                conectado = true;
+
+                                btConectar.setBackgroundColor(Color.GREEN);
+                                btConectar.setText("Desconectar");
+                                btConectar.setTextColor(Color.BLACK);
+                            } else {
+                                MensajesPantalla(4);
+                                conectado = false;
+                            }
+
+                            try {
+                                salidas = btsocket.getOutputStream();
+                                entradas = btsocket.getInputStream();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+
+                        } else {
+
+                        }
+                    }else{
+                        try {
+                            btsocket.close();
+                            conectado=false;
+
+                            btConectar.setBackgroundColor(Color.BLUE);
+                            btConectar.setText("Conectar");
+                            btConectar.setTextColor(Color.WHITE);
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
             }
         });
 
-        btPaired.setOnClickListener(new View.OnClickListener() {
-
+        btEnvio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                PairedDisp();
-                tvDisp.setText("Dispositivos Emparejados:\n" + dispositivosEmp + "\n\n" + numDisp + " Dispositivos emparejados");
-            }
 
-        });
+                if (conectado) {
+                    if (etEnvio.getText().length() > 0) {
 
-        btBuscar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PairedDisp();
-                if (macMicro.length() > 0) {
-                    tvMAC.setText("Mac " + nombreMicro + ": " + macMicro);
-                } else {
-                    tvMAC.setText("Microcontrolador no encontrado");
+                        String textoEnvio;
+                        int caracter;
+                        textoEnvio = String.valueOf(etEnvio.getText());
+
+                        try {
+
+                            for (caracter = 0; caracter < textoEnvio.length(); caracter++) {
+                                salidas.write(textoEnvio.charAt(caracter));
+                            }
+
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }else{
+                        MensajesPantalla(1);
+                    }
+                }else {
+                    MensajesPantalla(0);
                 }
-
             }
         });
 
-        btConectar.setOnClickListener(new View.OnClickListener() {
+        btRecibe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                if (conectado) {
+                    String cadenaRecibida = "";
+                    try {
 
-                Conectar(microControlador);
+                        entradas.skip(entradas.available());
+                        for (int i = 0; i < 4; i++) {
 
+                            cadenaRecibida = cadenaRecibida +(char)entradas.read();
+
+
+                        }
+                        tvRecibo.setText(cadenaRecibida);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }else{
+                    MensajesPantalla(0);
+                }
             }
         });
+
 
     }
+}
 
-
-    @SuppressLint("MissingPermission")
-    public void Conectar(BluetoothDevice device) {
-
-
-        mmDevice = device;
-
-        try {
-            mmSocket = bluetoothAdapter.listenUsingRfcommWithServiceRecord("BLDC_control", myUUID);
-        } catch (IOException e) {
-            tvMensaje.setText("");
-            tvDisp.setText("");
-            tvMAC.setText("MAL1");
-            Log.e("MIS MENSAJES", "error 1", e);
-        }
-
-        //RUN//
-        BluetoothSocket socket = null;
-        int salir=0;
-        // Keep listening until exception occurs or a socket is returned.
-        while (salir==0) {
-            try {
-                socket = mmSocket.accept();
-            } catch (IOException e) {
-                //Log.e(TAG, "Socket's accept() method failed", e);
-                tvMensaje.setText("");
-                tvDisp.setText("");
-                tvMAC.setText("MAL2");
-                break;
-            }
-
-            if (socket != null) {
-                // A connection was accepted. Perform work associated with
-                // the connection in a separate thread.
-                //manageMyConnectedSocket(socket);
-                try {
-                    mmSocket.close();
-                    tvMensaje.setText("");
-                    tvDisp.setText("");
-                    salir=1;
-                    tvMAC.setText("Conectado?");
-                } catch (IOException e) {
-                    tvMensaje.setText("");
-                    tvDisp.setText("");
-                    tvMAC.setText("MAL3");
-                }
-                break;
-            }
-
-        }
-
-
-
-        }
-
-    }
